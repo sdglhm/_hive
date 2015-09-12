51d4b29e827aa27c665455b45b14209a5fc0d916
sdglhm <lahiru.lahirumadusanka@gmail.com>
2015-09-12 16:49:03 +0530

    Initial _s upload

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package hive
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_hive' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_hive' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_hive' ), '_hive', '<a href="http://sdglhm.com" rel="designer">Lahiru Himesh Madusanka</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
